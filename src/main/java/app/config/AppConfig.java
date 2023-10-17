5c230fd73cccc14e2e9b45599bec1954effb916b
alexdraka <604.syndrome@gmail.com>
2023-10-18 02:17:38 +0300

    first commit

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -1,9 +1,17 @@
 package app.config;
 
+import app.model.Dog;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 @ComponentScan(basePackages = "app")
 public class AppConfig {
+
+    @Bean(name="dog")
+    public Dog getDog() {
+        return new Dog();
+    }
 }
